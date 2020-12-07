c93936fa9e552c59e32e9036d82b503901045434
alienboy13 <36003661+Sanjuro33@users.noreply.github.com>
2020-12-07 13:23:08 -0600

    Final organization was made and unnecesary scripts were removed

--- Assets/Scripts/CircuitComponent.cs
+++ Assets/Scripts/CircuitComponent.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class CircuitComponent : MonoBehaviour
 {
     [SerializeField] public float paths;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
